067737365d31eacac853aa437a239aaecf10e731
Joshua McKenna <josh@haru-mbp.ocad.ca>
2018-12-05 19:22:56 -0500

    DEC 5

--- sketch.js
+++ sketch.js
@@ -1,134 +1,253 @@
-var sketch1 = function(p) {
 
 
-        // *** ~~~ *** ~~~ *** POSTER ONE *** ~~~ *** ~~~ *** //
-        // * ~ Variables ~ * //
-
-        let timer = 1;
-        var margin = 10;
-        // Suprematism Background Colors
-        var backgroundArray0 = [p.color(232, 228, 223),    // Gray
-                                p.color(237, 237, 231),    // Light Gray
-                                p.color(252, 252, 246),    // Lighter Gray
-                                p.color(255, 254, 228),    // Light Yellow
-                                p.color(218, 202, 167),    // Dark Tan
-                                p.color(197, 181, 158),    // Brown
-                                p.color(197, 181, 158)];   // Brick Red
-        // Choose one randomly
-        var randBackground0 = backgroundArray0[Math.floor(Math.random() * backgroundArray0.length)]
-
-        // Suprematism Element Colors
-        var elementArray0 = [p.color(206, 78, 41),         // Orange
-                             p.color(39, 65, 106),         // Navy Blue
-                             p.color(0)]
-        // Choose one randomly
-        var randElementArray0 = elementArray0[Math.floor(Math.random() * elementArray0.length)]
-        var randElementArray1 = elementArray0[Math.floor(Math.random() * elementArray0.length)]
-
-        // Element Rotate Array
-        var rotateArray = [45, 90, 135, 180, 225, 280]
-
-        var middleXCanvas1 = (margin + (p.width/3)-(margin*3)) - (p.width/3)/2
-        var middleYCanvas1 = (margin + p.height) - p.height/2
-
-        var sVari = p.random(1, 8)
-        var rectH1 = p.random(5, 15)
-        var rectL1 = p.random(60, 100)
-        var rectX1 = p.random(20, 200)
-        var rectY1 = p.random(20, 500)
-
-        var degVari1 = p.random(0, 360)
-
-    // SETUP SKETCH ONE
-
-    p.setup = function() {
-        var canvas = p.createCanvas((p.windowWidth/3)-30, p.windowHeight -20);
-        canvas.parent('#sketch1');
-    };
-
-    p.draw = function() {
+function setup() {
+  createCanvas(windowWidth/3,windowHeight);
+}
 
 
 
-      // DRAW Poster ONE
-        // background
-        if (timer > 1) {
-        p.fill(randBackground0);
-        p.noStroke();
-        p.rect(0, 0, p.width, p.height);
-        }
-
-        // Element 1
-        if (timer > 2) {
-        p.push();
-        p.fill(randElementArray0);
-        p.translate(middleXCanvas1, middleYCanvas1);
-        p.rotate(degVari1);
-        p.scale(sVari);
-        p.triangle(30, 75, 58, 20, 86, 75);
-        p.pop();
-        }
-
-        // Element 2
-        if (timer > 3) {
-        p.push();
-        p.fill(randElementArray1);
-        p.rect(rectX1, rectY1, rectH1, rectL1);
-        p.rotate(degVari1);
-        p.pop();
-        }
-
-        console.log(timer)
-
-        if (p.frameCount % 60 == 0 && timer > 0) {
-          console.log("wtf")
-          timer++;
-          console.log(timer)
-        }
-
-
-// counter that increases from 0 to length of array and then loops background (specifies which part of the array to call)
-// counter that resets everytime it reaches 60 (or framecount is divisible % 0) while also calling the function to use with array
-// timeCounterMax (to change how fast the array counter works)
+function draw () {
+  frameRate(1);
+  backgroundDraw();
+  triangleDraw();
+  quadDraw();
+  quadDraw();
+  rectangleDraw();
+  circleDraw();
+}
 
 
 
+function backgroundDraw () {
+  var backgroundArray =  [color(232, 228, 223),     // Gray
+                          color(237, 237, 231),    // Light Gray
+                          color(252, 252, 246),    // Lighter Gray
+                          color(255, 254, 228),    // Light Yellow
+                          color(218, 202, 167),    // Dark Tan
+                          color(197, 181, 158),    // Brown
+                          color(197, 181, 158),     // Brick Red
+                          color(219, 212, 193)]     // Sand
+
+  backgroundColor = backgroundArray[Math.floor(Math.random() * backgroundArray.length)]
+  background(backgroundColor);
+}
+
+function circleDraw () {
+  var elementColorArray =  [color(206, 78, 41),        // Orange
+                            color(39, 65, 106),       // Navy Blue
+                            color(0),                 // Black
+                            color(26, 65, 25),       // Forest Green
+                            color(245, 227, 212),     // pink
+                            color(35, 31, 32),        // off black
+                            color(248, 207, 17),      // yellow
+                            color(118, 1, 8),         // maroon
+                            color(95, 69, 44),        // brown
+                            color(110, 160, 100)]     // green
+
+  circleColor = elementColorArray[Math.floor(Math.random() * elementColorArray.length)]
+
+  var circleArray = [1, 2];
+  var circleChoice = circleArray[Math.floor(Math.random() * circleArray.length)]
+
+  if (circleChoice == 1) {
+
+    var radius = random(20, 100)
+
+    push();
+    noStroke();
+    fill(quadColor);
+    translate(random(0, width), random(0, height));
+    ellipse(20, 20, radius, radius);
+    pop();
+  }
+
+  if (circleChoice == 2 && rectChoice == 2 && quadChoice == 3) {
+
+    var radius = random(250, 300)
+
+    push();
+    noStroke();
+    fill(quadColor);
+    translate(random(0, width), random(0, height));
+    ellipse(20, 20, radius, radius);
+    pop();
+  }
+}
+
+function rectangleDraw () {
+  angleMode(DEGREES);
+  var elementColorArray =  [color(206, 78, 41),        // Orange
+                            color(39, 65, 106),       // Navy Blue
+                            color(0),                 // Black
+                            color(26, 65, 25),       // Forest Green
+                            color(245, 227, 212),     // pink
+                            color(35, 31, 32),        // off black
+                            color(248, 207, 17),      // yellow
+                            color(118, 1, 8),         // maroon
+                            color(95, 69, 44),        // brown
+                            color(110, 160, 100)]     // green
+
+  quadColor = elementColorArray[Math.floor(Math.random() * elementColorArray.length)]
+
+  var rectArray = [1, 2];
+  rectChoice = rectArray[Math.floor(Math.random() * rectArray.length)]
+
+  if (rectChoice == 1) {
+
+    push();
+    noStroke();
+    fill(quadColor);
+    translate(random(0, width), random(0, height));
+    rotate(random(0,360));
+    translate(-29, -287);
+  	rect(26,56,8,459);
+    pop();
+
+    push();
+    noStroke();
+    fill(quadColor);
+    translate(random(0, width), random(0, height));
+    rotate(random(0,360));
+    translate(-23, -123);
+    rect(17,21,10,205);
+    pop();
+
+    var toggleArray = [1, 2];
+    toggle = toggleArray[Math.floor(Math.random() * toggleArray.length)]
+
+    if (toggle == 1) {
+      push();
+      noStroke();
+      fill(quadColor);
+      translate(random(0, width), random(0, height));
+      rotate(random(0,360));
+      translate(-23, -123);
+      rect(17,21,10,205);
+      pop();
+
+      push();
+      noStroke();
+      fill(quadColor);
+      translate(random(0, width), random(0, height));
+      rotate(random(0,360));
+      translate(-25, -87);
+      rect(19,21,17,134);
+      pop();
+    }
+  }
+}
 
 
-    };
-};
 
-var sketch2 = function(p) {
-    p.setup = function() {
-        var canvas = p.createCanvas((p.windowWidth/3)-30, p.windowHeight -20);
-        canvas.parent('#sketch2');
-    };
+function quadDraw () {
+  angleMode(DEGREES);
+  var elementColorArray =  [color(206, 78, 41),        // Orange
+                            color(39, 65, 106),       // Navy Blue
+                            color(0),                 // Black
+                            color(26, 65, 25),        // Forest Green
+                            color(245, 227, 212),     // pink
+                            color(35, 31, 32),        // off black
+                            color(248, 207, 17),      // yellow
+                            color(118, 1, 8),         // maroon
+                            color(95, 69, 44),        // brown
+                            color(110, 160, 100)]     // green
+  quadColor = elementColorArray[Math.floor(Math.random() * elementColorArray.length)]
 
-    p.draw = function() {
-        p.background(0, 0, 51);
-        p.fill(0, 255, 0);
-        // answers question 2
-        p.rectMode(p.CENTER);
-        p.rect(p.width / 2, p.height / 2, 1000, 50);
-    };
-};
+  var quadArray = [1, 2, 3];
+  quadChoice = quadArray[Math.floor(Math.random() * quadArray.length)]
 
-var sketch3 = function(p) {
-    p.setup = function() {
-        var canvas = p.createCanvas((p.windowWidth/3)-30, p.windowHeight -20);
-        canvas.parent('#sketch3');
-    };
+  if (quadChoice == 1 && triangleChoice == 4) {
+    push();
+    noStroke();
+    fill(quadColor);
+    translate(random(0, width), random(0, height));
+    rotate(random(0,360));
+    translate(-288, -330);
+    scale(random(1,1.1))
+  	quad(482,509,150,100,113,143,389,558);
+    pop();
+  }
 
-    p.draw = function() {
-        p.background(0, 0, 51);
-        p.fill(0, 255, 0);
-        // answers question 2
-        p.rectMode(p.CENTER);
-        p.rect(p.width / 2, p.height / 2, 1000, 50);
-    };
-};
+  if (quadChoice == 2) {
+    push();
+    noStroke();
+    fill(quadColor);
+    translate(random(0, width), random(0, height));
+    rotate(random(0,360));
+    translate(-330, -319);
+    scale(random(1,1.1))
+    quad(192,295,375,174,487,352,259,451);
+    pop();
+  }
+}
+
+
+
+function triangleDraw () {
+  angleMode(DEGREES);
+  var elementColorArray =  [color(206, 78, 41),        // Orange
+                            color(39, 65, 106),       // Navy Blue
+                            color(0),                 // Black
+                            color(26, 65, 25),        // Forest Green
+                            color(245, 227, 212),     // pink
+                            color(35, 31, 32),        // off black
+                            color(248, 207, 17),      // yellow
+                            color(118, 1, 8),         // maroon
+                            color(95, 69, 44),        // brown
+                            color(110, 160, 100)]     // green
+  triangleColor = elementColorArray[Math.floor(Math.random() * elementColorArray.length)]
+
+  var triangleArray = [1, 2, 3, 4];
+  triangleChoice = triangleArray[Math.floor(Math.random() * triangleArray.length)]
+
+  if (triangleChoice == 1) {
+    push();
+    noStroke();
+    fill(triangleColor);
+    translate(random(0, width), random(0, height));
+    rotate(random(0,360));
+    translate(-111, -285);
+    scale(random(1,1.2))
+    triangle(16,389,259,284,16,183);
+    pop();
+  }
+
+  if (triangleChoice == 2) {
+    push();
+    noStroke();
+    fill(triangleColor);
+    translate(random(0, width), random(0, height));
+    rotate(random(0,360));
+    translate(-174, -247);
+    scale(random(1,1.1))
+    triangle(40,347,309,346,166,91);
+    pop();
+  }
+
+  if (triangleChoice == 3) {
+    push();
+    noStroke();
+    fill(triangleColor);
+    translate(random(0, width), random(0, height));
+  	rotate(random(0, 360));
+  	translate(-214, -201);
+    scale(random(1,1.1))
+  	triangle(281,379,280,3,2,379);
+    pop();
+  }
+
+  if (triangleChoice == 4) {
+    push();
+    noStroke();
+    fill(triangleColor);
+    translate(random(0, width), random(0, height));
+  	rotate(random(0, 360));
+    translate(-26, -63);
+    scale(random(1,1.3))
+    triangle(5,112,32,112,31,1);
+    pop();
+  }
+}
 
 
 var myFirstSketch = new p5(sketch1);
-var mySecondSketch = new p5(sketch2);
-var myThirdSketch = new p5(sketch3);
